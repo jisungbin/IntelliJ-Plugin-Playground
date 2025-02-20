e8c2e6cb890bda34faca8b36ab3fbab7f0c29a42
Ji Sungbin <ji@sungb.in>
2025-02-20 18:19:37 +0900

    clean

--- src/main/kotlin/land/sungbin/intellij/playground/names.kt
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 Ji Sungbin
-// SPDX-License-Identifier: Apache-2.0
-package land.sungbin.intellij.playground
-
-import org.jetbrains.kotlin.name.FqName
-
-internal val COMPOSE_COLOR_FQN = FqName("androidx.compose.ui.graphics.Color")
